1906c31bfeb778fd83f19570d202773d263b9d40
lkf6214 <lkf6214@naver.com>
2023-08-20 23:11:53 +0900

    헤더 js inclueHTML.js에 넣기

--- includeHTML.js
+++ includeHTML.js
@@ -35,15 +35,36 @@ function includeHTML(callback) {
   //   callback();
   // }, 0);
 
-
   const search_form = document.querySelector('.search_form');
   const search_form_input = document.querySelector('.search_form input');
 
-  search_form.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    let search_value = search_form_input.value
+  search_form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let search_value = search_form_input.value;
     location.href = `/project_list/project_list.html?${search_value}`;
-  
-  })
+  });
 
+  //header hamburger click
+  const mobileNavIcon = document.querySelector('.hamburger');
+  const navList = document.querySelector('.nav_list');
+  const closeBtn = document.querySelector('.btn_close');
+  console.log(closeBtn);
+
+  // 햄버거 아이콘 클릭 시 네비게이션 목록 토글
+  mobileNavIcon.addEventListener('click', function () {
+    navList.classList.toggle('active');
+  });
+
+  // 문서 전체에 대한 클릭 이벤트 리스너 추가
+  document.addEventListener('click', function (event) {
+    const target = event.target;
+
+    // 클릭한 영역이 네비게이션 목록과 햄버거 아이콘 모두 아닐 경우 네비게이션 목록 닫기
+    if (
+      (!navList.contains(target) && !mobileNavIcon.contains(target)) ||
+      closeBtn.contains(target)
+    ) {
+      navList.classList.remove('active');
+    }
+  });
 }
